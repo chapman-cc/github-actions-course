0c58e332edb685bbd2f2eda7c49dd4c8590e0a9e
Chapman Cheng <45029601+chapman-cc@users.noreply.github.com>
2025-10-26 22:32:21 +0000

    17-js-actions-3

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,55 @@
 const core = require("@actions/core");
+const { exec, getExecOutput } = require("@actions/exec");
+
+const validateBranchName = ({ branchName }) =>
+  /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const validateDirectoryName = ({ dirName }) =>
+  /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
 async function run() {
-  core.info("I am a custom js function");
+  const baseBranch = core.getInput("base-branch");
+  const targetBranch = core.getInput("target-branch");
+  const workingDir = core.getInput("working-directory");
+  const ghToken = core.getInput("gh-token");
+  const debug = core.getBooleanInput("debug");
+
+  core.setSecret(ghToken);
+
+  if (!validateBranchName({ branchName: baseBranch })) {
+    core.setFailed(
+      "Invalid base-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes."
+    );
+    return;
+  }
+
+  if (!validateBranchName({ branchName: targetBranch })) {
+    core.setFailed(
+      "Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes."
+    );
+    return;
+  }
+  if (!validateDirectoryName({ dirName: workingDir })) {
+    core.setFailed(
+      "Invalid working directory name. Directory names should include only characters, numbers, hyphens, underscores, and forward slashes."
+    );
+    return;
+  }
+
+  core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
+  core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
+  core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+
+  await exec("npm update", [], { cwd: workingDir });
+
+  const gitStatus = await getExecOutput("git status -s package*.json", [], {
+    cwd: workingDir,
+  });
+  if (gitStatus.stdout.length > 0) {
+    core.info("[js-dependency-update] : There are updates available!");
+  } else {
+    core.info("[js-dependency-update] : No updates at this point in time.");
+  }
+  core.info("I am a custom JS action");
 }
 
 run();
